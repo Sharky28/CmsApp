d84ff22730f3d5647af1c8441b233cd986081dca
SharkyUgas <sharmarke.ugas@outlook.com>
2019-07-02 14:33:36 +0100

    added overflow menu on right

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule}  from '@angular/material/button';
 import {MatIconModule}  from '@angular/material/icon';
 import {MatSidenavModule}  from '@angular/material/sidenav';
-import { MatToolbarModule} from '@angular/material/toolbar';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import{ MatMenuModule} from '@angular/material/menu';
 //Routing
 import { AppRoutingModule} from './app.routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { AppComponent } from './app.component';
     MatButtonModule,
     MatIconModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatMenuModule
    
   ],
   providers: [],
