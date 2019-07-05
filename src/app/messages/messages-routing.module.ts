94291642243f927144516f40129b87280916e997
SharkyUgas <sharmarke.ugas@outlook.com>
2019-07-05 12:06:16 +0100

    added linear steppers for message creation

--- src/app/messages/messages-routing.module.ts
+++ src/app/messages/messages-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MessageListComponent } from './message-list/message-list.component';
-
+import { MessageNewComponent} from './message-new/message-new.component';
 const routes: Routes = [
   {
     path:'',
     component: MessageListComponent
+  },
+  {
+    path:'',
+    component: MessageNewComponent
   }
 ];
 @NgModule({
